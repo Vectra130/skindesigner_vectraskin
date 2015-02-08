faf52e614b284fe8c4b46b370a608232e96f6dc2
Vectra130 <vectra130@online.de>
2015-02-08 23:11:23 +0100

    test

--- scripts/skindesigner_infos.sh
+++ scripts/skindesigner_infos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v1.0 eeebox
+# v1.1 eeebox
 
 #Infos fuer den SkinDesigner
 
@@ -7,38 +7,22 @@ DBUS="/usr/bin/vdr-dbus-send.sh /Plugins/skindesigner plugin.SVDRPCommand string
 OUTPUTFLDR=/tmp/skindesigner
 mkdir -p $OUTPUTFLDR
 
-#cpu usage
-#CPUUSAGE=$(printf "%.0f\n" $(cat /proc/stat | grep '^cpu ' | awk '{ print ($2+$4)*100/($2+$4+$5) }' | sed -e 's/[.]/,/g'))
-#CPUUSAGE=$(cat /proc/stat | grep '^cpu ' | awk '{ print ($2+$4)*100/($2+$4+$5) }' | awk -F'.' '{ print $1 }' )
-#$DBUS string:"ctCpuUsage = $(( $(echo $CPUUSAGE  | sed -e 's/[,]/./g') + 1 ))"
-#[ $CPUUSAGE -gt 100 ] && CPUUSAGE=100
-#echo "${CPUUSAGE}.0" > ${OUTPUTFLDR}/cpuusage
-#logger -t INFO "CPUUSAGE "$CPUUSAGE
-
 #mem usage
 MEMUSAGE=$(free | grep "Mem:" | awk '{ print $3*100/$2 }'  | awk -F'.' '{ print $1 }' )
 [ $MEMUSAGE -gt 100 ] && MEMUSAGE=100
 $DBUS string:"ctMem = ${MEMUSAGE}"
-#echo "${MEMUSAGE}.0" > ${OUTPUTFLDR}/memusage
-#logger -t INFO "MEMUSAGE "$MEMUSAGE
 
 #swap usage
 #SWAPUSAGE=$(free | grep "Swap:" | awk '{ print ($2-$4)*100/$2 }'  | awk -F'.' '{ print $1 }' )
-#$DBUS string:"ctSwapUsage = $(( $(echo $SWAPUSAGE  | sed -e 's/[,]/./g') + 1 ))"
-#logger -t INFO "SWAPUSAGE "$SWAPUSAGE
+#[ $SWAPUSAGE -gt 100 ] && SWAPUSAGE=100
+#$DBUS string:"ctSwap = ${SWAPUSAGE}"
 
 #cpu temp
 CPUTEMP=$(sensors -A coretemp-isa-0000 | grep "Core 0" | awk '{print $3}' | tr -d "+"  | awk -F'.' '{ print $1 }')
 [ $CPUTEMP -gt 80 ] && CPUTEMP=80
-echo "${CPUTEMP}.0" > ${OUTPUTFLDR}/cpu
+echo "${CPUTEMP}°C" > ${OUTPUTFLDR}/cpu
 
 #gpu temp
 GPUTEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader  | awk -F'.' '{ print $1 }')
 [ $GPUTEMP -gt 80 ] && GPUTEMP=80
-echo "${GPUTEMP}.0" > ${OUTPUTFLDR}/gpu
-
-#cpuload
-#CPULOAD=$(cat /proc/loadavg | awk '{ print $1 }')
-#CPULOAD=$(echo "$CPULOAD * 100" | bc | awk -F'.' '{ print $1 }')
-#[ $CPULOAD -gt 250 ] && CPULOAD=250
-#echo "${CPULOAD}.0" > ${OUTPUTFLDR}/cpuusage
+echo "${GPUTEMP}°C" > ${OUTPUTFLDR}/gpu
